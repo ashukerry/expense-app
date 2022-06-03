67e138b18faa12b42be7c22bff101e5f7ea7b4ae
ashukerry <yourgitemail@example.com>
2022-06-03 04:44:33 +0100

    extracting the business logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,24 +6,25 @@ import {
   Delete,
   Param,
   Body,
+  HttpCode,
 } from '@nestjs/common';
 import { data, ReportType } from './data';
 import { v4 as uuid } from 'uuid';
+import { AppService } from './app.service';
 @Controller('report/:type')
 export class AppController {
+  constructor(private readonly appService: AppService) {}
   @Get()
   getAllReports(@Param('type') type: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report.filter((report) => report.type === reportType);
+    return this.appService.getAllReports(reportType);
   }
   @Get(':id')
   getReportById(@Param('type') type: string, @Param('id') id: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report
-      .filter((report) => report.type == reportType)
-      .find((report) => report.id === id);
+    return this.appService.getReportById(reportType, id);
   }
   @Post()
   createReport(
@@ -38,22 +39,27 @@ export class AppController {
     },
     @Param('type') type: string,
   ) {
-    const newReport = {
-      id: uuid(),
-      source,
-      amount,
-      created_at: new Date(),
-      updated_at: new Date(),
-      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
-    };
-    return 'created';
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, { amount, source });
   }
   @Put(':id')
-  upDateReport() {
-    return 'updated';
+  upDateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body()
+    body: {
+      amount: number;
+      source: string;
+    },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.upDateReport(reportType, id, body);
   }
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
-    return 'delete';
+  deleteReport(@Param('id') id: string) {
+    return this.appService.delete(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { data, ReportType } from './data';
+import { v4 as uuid } from 'uuid';
 
+interface Report {
+  amount: number;
+  source: string;
+}
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: ReportType) {
+    // instead of having this logic in the controller, we will call the method in there
+    return data.report.filter((report) => report.type === type);
+  }
+  getReportById(type: ReportType, id: string) {
+    return data.report
+      .filter((report) => report.type == type)
+      .find((report) => report.id === id);
+  }
+  createReport(type: ReportType, { amount, source }: Report) {
+    const newReport = {
+      id: uuid(),
+      amount,
+      source,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+  upDateReport(type: ReportType, id: string, body: Report) {
+    const reportToUpdate = data.report
+      .filter((report) => report.type == type)
+      .find((report) => report.id === id);
+    if (!reportToUpdate) return;
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+      updated_at: new Date(),
+    };
+    return data.report[reportIndex];
+  }
+  delete(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id == id);
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
+    return;
   }
 }
